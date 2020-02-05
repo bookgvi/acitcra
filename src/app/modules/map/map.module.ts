5d81ea3e02c78336f6ea8a37c250eb7aa348db5c
book <bookgvi@gmail.com>
2020-02-06 00:06:13 +0300

    сервис по работе с маркерами на карте

--- src/app/modules/map/map.module.ts
+++ src/app/modules/map/map.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 
+import { MarkersService } from '../../services/markers.service';
+
 import { MapBaseComponent } from '../../components/map/map-base/map-base.component';
 
 @NgModule({
   declarations: [
     MapBaseComponent
+  ],
+  providers: [
+    MarkersService
   ],
   imports: []
 })
