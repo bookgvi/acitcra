572265ad2bc246bcaa412abef657a2558f19437d
book <bookgvi@gmail.com>
2020-02-07 01:34:10 +0300

    Инфо панель, регионы

--- src/app/components/map/map-base/map-base.component.ts
+++ src/app/components/map/map-base/map-base.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit, AfterViewInit, EventEmitter } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
-import { MarkersService } from '../../../services/markers.service';
+
+import { MarkersService } from '../../../services/markers/markers.service';
+import { MapService } from '../../../services/map/map.service';
+import { ShapesService } from '../../../services/shapes/shapes.service';
 
 @Component({
   selector: 'app-map-base',
@@ -9,24 +12,40 @@ import { MarkersService } from '../../../services/markers.service';
 })
 export class MapBaseComponent implements OnInit, AfterViewInit {
   private map;
-  private moscowCoords: Number[];
+  private moscowCoords: number[];
+  private centerOfRussia: number[];
+  private baseZoom: number;
+  private maxZoom: number;
+  private clickZoom: number;
+  private subjectsOfRussia: string;
+  private RussiaBoundsLeftTop: number[];
+  private RussiaBoundsRightBottom: number[];
 
   constructor(
-    private marker: MarkersService
+    private marker: MarkersService,
+    private mapService: MapService,
+    private shapes: ShapesService
   ) {
     this.moscowCoords = [55.751244, 37.618423];
+    this.centerOfRussia = [67.58290340170387, 105.2480033085533];
+    this.baseZoom = 4;
+    this.maxZoom = 19;
+    this.clickZoom = 11;
+    this.subjectsOfRussia = '../../assets/data/Regions.geojson';
+    this.RussiaBoundsLeftTop = [82.04574006217713, 17.402343750000004];
+    this.RussiaBoundsRightBottom = [39.095962936305476, 187.73437500000003];
   }
 
   private initMap(): void {
-    this.map = L.map('map', {
-      center: this.moscowCoords,
-      zoom: 4
+    return L.map('map', {
+      center: this.centerOfRussia,
+      zoom: this.baseZoom
     });
   }
 
   private addTiles(): void {
     const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19
+      maxZoom: this.maxZoom
     });
 
     tiles.addTo(this.map);
@@ -36,12 +55,31 @@ export class MapBaseComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-    this.initMap();
-    this.addTiles();
+    this.map = this.initMap();
+    this.map.fitBounds([this.RussiaBoundsLeftTop, this.RussiaBoundsRightBottom]);
+    // this.addTiles();
     const mosCenterMarker = this.marker.initMarker(this.moscowCoords);
     this.marker.setStartingMarker(mosCenterMarker, this.map);
-    this.marker.setMarkerOnClick(this.map);
 
+
+    /**
+     * Вспомогательные методы, периодически используются
+     *
+     * */
+    // this.marker.setMarkerOnClick(this.map);
+    // this.mapService.centerMapOnClick(this.map, this.baseZoom);
+    //
+    // // Рисуем границу РФ
+    // this.shapes.getShape().subscribe(shape => {
+    //   const shapeLayer = this.shapes.initShapes(shape);
+    //   this.map.addLayer(shapeLayer);
+    // });
+
+    this.shapes.initInfoPanel(this.map);
+    // Рисуем субъекты РФ
+    this.shapes.getShape(this.subjectsOfRussia).subscribe(shape => {
+      const shapeLayer = this.shapes.initClickableShapes(shape, this.map);
+      this.map.addLayer(shapeLayer);
+    });
   }
-
 }
---
+++ src/app/services/map/map.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class MapService {
+  private coordsOfClick: number[];
+  constructor() { }
+
+  public centerMapOnClick(map, zoom): void {
+    map.on('click', e => {
+      this.coordsOfClick = [e.latlng.lat, e.latlng.lng];
+      map.setView(this.coordsOfClick, zoom);
+    });
+    //
+  }
+}
