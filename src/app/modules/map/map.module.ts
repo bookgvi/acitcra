8cb7472bcf90b67829eca201a39e03382dbb2f98
Виатлий <vitaliy.guselnikov@ucann.ru>
2020-02-07 14:34:27 +0300

    ARCTICA-3: стилизация субъектов в АЗРФ

--- src/app/modules/map/map.module.ts
+++ src/app/modules/map/map.module.ts
@@ -3,7 +3,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { MarkersService } from '../../services/markers/markers.service';
 import { MapService } from '../../services/map/map.service';
-import { ShapesService } from '../../services/shapes/shapes.service';
+import { ShapesService } from '../../services/subjectsShapes/shapes.service';
 
 import { MapBaseComponent } from '../../components/map/map-base/map-base.component';
 
