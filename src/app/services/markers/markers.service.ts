3269b49584374814dcd38a9674fcb688aa811a53
book <bookgvi@gmail.com>
2020-02-08 19:41:16 +0300

    ARCTICA-3: tsdoc

--- src/app/services/markers/markers.service.ts
+++ src/app/services/markers/markers.service.ts
@@ -6,14 +6,29 @@ export class MarkersService {
   constructor() {
   }
 
-  initMarker(coords: number[]) {
+  /**
+   *
+   * Инициализация маркера
+   *
+   * @param coords - массив с координатами [longitude, latitude]
+   *
+   */
+  initMarker(coords: number[]): object {
     const marker = new L.Marker(coords, {
       draggable: true
     });
     return marker;
   }
 
-  setStartingMarker(marker, map) {
+  /**
+   *
+   * Установка маркера в центре Москвы (по нажатию подсказка "Москва")
+   *
+   * @param marker
+   * @param map
+   *
+   */
+  setStartingMarker(marker, map): void {
     marker.bindPopup('Moscow').openPopup();
     marker.addTo(map);
     marker.on('contextmenu', e => {
@@ -21,6 +36,13 @@ export class MarkersService {
     });
   }
 
+  /**
+   *
+   * Установка маркера на карте и вывод в консоль координат при нажатии левой кнопкой мыши
+   * Удаление маркера с карты по нажатию на нем правой кнопкой мыши
+   *
+   * @param map
+   */
   setMarkerOnClick(map) {
     map.on('click', (e) => {
       console.log([e.latlng.lat, e.latlng.lng]);
